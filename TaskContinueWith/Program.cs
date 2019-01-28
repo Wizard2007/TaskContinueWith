550d024f4d25c3f3725b745f75c39811c4bb0efd
Eujine <sponsor85@mail.ru>
2019-01-28 23:39:52 +0200

    init commit

---
+++ TaskContinueWith/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskContinueWith
+{
+    class Program
+    {
+        static Stopwatch watch = new Stopwatch();
+        static int pendingTasks;
+        static void Main(string[] args)
+        {
+            const int MaxValue = 1000000000;
+            watch.Restart();
+            int numTasks = Environment.ProcessorCount;
+            pendingTasks = numTasks;
+            int perThreadCount = MaxValue / numTasks;
+            int perThreadLeftover = MaxValue % numTasks;
+            var tasks = new Task<long>[numTasks];
+            for (int i = 0; i < numTasks; i++)
+            {
+                int start = i * perThreadCount;
+                int end = (i + 1) * perThreadCount;
+                if (i == numTasks - 1)
+                {
+                    end += perThreadLeftover;
+                }
+                tasks[i] = Task<long>.Run(() =>
+                {
+                    long threadSum = 0;
+                    for (int j = start; j <= end; j++)
+                    {
+                        threadSum += (long)Math.Sqrt(j);
+                    }
+                    return threadSum;
+                });
+                tasks[i].ContinueWith(OnTaskEnd, TaskContinuationOptions.ExecuteSynchronously);
+            }
+            Console.WriteLine("Main Completed!");
+            Console.ReadKey();
+        }
+        private static async void OnTaskEnd(Task<long> task)
+        {
+            Console.WriteLine("Thread sum: {0}", await task);
+            if (Interlocked.Decrement(ref pendingTasks) == 0)
+            {
+                watch.Stop();
+                Console.WriteLine("Tasks: {0}", watch.Elapsed);
+            }
+        }
+    }
+}
